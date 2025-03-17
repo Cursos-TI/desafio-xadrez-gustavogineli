f947adb1dea94bc37a2c912b15a2c4cdd0d8a905
Gustavo Gineli <112973442+gustavogineli@users.noreply.github.com>
2025-03-17 19:21:57 +0000

    modified

--- xadrez.c
+++ xadrez.c
@@ -57,7 +57,4 @@ int main() {
     // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
     // Inclua o uso de continue e break dentro dos loops.
 
-
-
-    return 0;
 }
